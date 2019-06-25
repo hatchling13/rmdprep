f20f630e58004cd6fa18ba7271754834a406decd
Hatchling <hatchling3713@gmail.com>
2019-06-25 23:28:22 +0900

    Changed README, added read_file function

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # rmdprep
 Markdown preprocessor written in Rust. Inspired from [this link](https://libsora.so/posts/maya-markdown-preprocessor-post-mortem)(Korean).
 ## MVP(Minimum Viable Project)
-1. Make outputs that fit with hugo's markdown syntax(Only because I'm going to use hugo to manage my website).
+1. Make outputs that fit with zola's markdown syntax(Only because I'm going to use zola to manage my website).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    let usage = "
+                Usage:\n
+                  rmdprep <file> [options]
+                Options\n
+                  -h --help\tShow this screen.\n
+                  -v --version\tShow version.\n
+                ";
+    */
+    
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[1];
+    let mut contents = String::new();
+
+    {
+      let result = read_file(file_name, &mut contents);
+
+      match result {
+        Ok(size) => println!("read_file succeeded, file size: {}", size),
+        Err(e) => print!("read_file failed: {:?}", e.kind()),
+      }
+    }
+
+    if !contents.is_empty() {
+      create_post(contents);
+    }
+}
+
+fn read_file(file_name: &str, contents: &mut String) -> std::result::Result<usize, std::io::Error> {
+  let file = File::open(file_name);
+  
+  match file {
+    Ok(_) => {
+      let mut file_buffer = BufReader::new(file.unwrap());
+      let result = file_buffer.read_to_string(contents);
+
+      result
+    }
+    Err(e) => {
+      println!("cannot open file: {:?}", e.kind());
+      return Err(e);
+    }
+  }
+}
+
+fn create_post(contents: String) {
+  
 }
