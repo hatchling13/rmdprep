010add791f51d3889dbd207fd6b7d219df355a1d
Hatchling <hatchling3713@gmail.com>
2019-06-27 00:09:25 +0900

    Not important, just tweaking

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,67 @@
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
+
+#[allow(dead_code)]
+enum StaticGen {
+    Zola,
+    Hugo,
+}
 
 fn main() {
     /*
     let usage = "
                 Usage:\n
-                  rmdprep <file> [options]
-                Options\n
-                  -h --help\tShow this screen.\n
-                  -v --version\tShow version.\n
-                ";
+                rmdprep <file> [options]
+              Options\n
+                -h --help\tShow this screen.\n
+                -v --version\tShow version.\n
+              ";
     */
-    
     let args: Vec<String> = env::args().collect();
-
     let file_name = &args[1];
-    let mut contents = String::new();
-
+    
+    let mut text = String::new();
+    
     {
-      let result = read_file(file_name, &mut contents);
-
-      match result {
-        Ok(size) => println!("read_file succeeded, file size: {}", size),
-        Err(e) => print!("read_file failed: {:?}", e.kind()),
-      }
+        let result = read_file(file_name, &mut text);
+        
+        match result {
+            Ok(size) => println!("read_file succeeded, file size: {}", size),
+            Err(e) => print!("read_file failed: {:?}", e.kind()),
+        }
     }
-
-    if !contents.is_empty() {
-      create_post(contents);
+    
+    if !text.is_empty() {
+        create_post(text);
     }
 }
 
 fn read_file(file_name: &str, contents: &mut String) -> std::result::Result<usize, std::io::Error> {
-  let file = File::open(file_name);
-  
-  match file {
-    Ok(_) => {
-      let mut file_buffer = BufReader::new(file.unwrap());
-      let result = file_buffer.read_to_string(contents);
-
-      result
+    let file = File::open(file_name);
+    
+    match file {
+        Ok(_) => {
+            let mut file_buffer = BufReader::new(file.unwrap());
+            let result = file_buffer.read_to_string(contents);
+        
+            result
+        }
+        Err(e) => {
+            println!("cannot open file: {:?}", e.kind());
+            return Err(e);
+        }
     }
-    Err(e) => {
-      println!("cannot open file: {:?}", e.kind());
-      return Err(e);
-    }
-  }
 }
 
-fn create_post(contents: String) {
-  
+fn create_post(s: String) {
+    // let mut content = String::new();
+
+    // TODO : Implement
+    // Divide text into front-matter and content
+
+    let a = s.rfind("\\+\\+\\+");
+
+    print!("Position of second +++ : {}", a.unwrap());
 }
