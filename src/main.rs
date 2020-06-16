ef4bffadcb047d624c005370c92219cccc2cdf57
Hatchling <hatchling3713@gmail.com>
2020-06-16 11:22:04 +0900

    enhancement#2 "Apply command line parser"
    Bit fishy to be fully approved, needs something more

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{App, load_yaml};
+
 fn main() {
-    println!("Hello, world!");
+
+    let _locale = "en";
+
+    // add macro to choose yml filename decided by locale
+
+    let yaml = load_yaml!("cli.yml");
+    let m = App::from(yaml).get_matches();
+
+    if m.is_present("INPUT") {
+        println!("{}", m.value_of("INPUT").unwrap());
+    }
 }
