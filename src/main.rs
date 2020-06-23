841988dfc084cf33d2c4709a1af192e339f5c5ed
Jeong Wook Park <hatchling3713@gmail.com>
2020-06-23 14:30:50 +0900

    Revert "enhancement#2 "Apply command line parser" (#3)"
    This reverts commit 5b3925f166033903d9e9222c7c104412c4f687ff.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-mod args;
-
 fn main() {
-    let m = args::new();
-
-    let mut input = String::new();
-
-    if m.is_present("INPUT") {
-        input = m.value_of("INPUT").unwrap().to_string();
-    }
-
-    // Can receive multiple filename at once
-    let names = input.split(',');
-
-    for name in names {
-        println!("{}", name);
-    }
+    println!("Hello, world!");
 }
