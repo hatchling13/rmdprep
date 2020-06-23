a745a56b2fae57106e263bdc2837a6c2fbc910e6
Jeong Wook Park <hatchling3713@gmail.com>
2020-06-23 14:34:12 +0900

    Enhancement#2 (#5)
    * enhancement#2 "Apply command line parser"
    
    Bit fishy to be fully approved, needs something more
    
    * Changed clap usage to builder pattern from yaml loading
    
    * Cleaned Cargo.toml up, changed 'files' into 'names' to get more clarity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod args;
+
 fn main() {
-    println!("Hello, world!");
+    let m = args::new();
+
+    let mut input = String::new();
+
+    if m.is_present("INPUT") {
+        input = m.value_of("INPUT").unwrap().to_string();
+    }
+
+    // Can receive multiple filename at once
+    let names = input.split(',');
+
+    for name in names {
+        println!("{}", name);
+    }
 }
